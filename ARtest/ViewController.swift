b45ebd1757110cd5855f46a26b075eaa5452f501
codegangsk <kjhj0604@gmail.com>
2020-08-25 22:49:16 +0900

    Initial Commit

---
+++ ARtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARtest
+//
+//  Created by Sophie Kim on 2020/08/25.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
