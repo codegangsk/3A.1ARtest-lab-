f2d59da55701394b40b6324a77409e409386544f
codegangsk <kjhj0604@gmail.com>
2020-08-26 17:42:55 +0900

    first commit

--- ARtest/ViewController.swift
+++ ARtest/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, ARSCNViewDelegate{
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let config = ARWorldTrackingConfiguration()
+    config.planeDetection = .horizontal
+    sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
+    sceneView.delegate = self
+    sceneView.session.run(config)
+    addBox()
     }
-
-
+    
+    func addBox(){
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.blue
+        box.materials = [material]
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        boxNode.position = SCNVector3(0, 0, -0.2)
+        let scene = SCNScene()
+        scene.rootNode.addChildNode(boxNode)
+        sceneView.scene = scene
+    }
 }
 
